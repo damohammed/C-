ac2270a6d2756917bd44c7b649235fad5aaacd2f
mo7amad999999@ <mo7amad999999@gmail.com>
2021-12-22 14:47:44 +0500

    new file

---
+++ lops.sh
@@ -0,0 +1,16 @@
+arrayName=($*)
+max=${arrayName[0]}
+min=${arrayName[0]}
+for i in "${arrayName[@]}"
+do
+    if [[ "$i" -gt "$max" ]]; then
+        max="$i"
+    fi
+
+    if [[ "$i" -lt "$min" ]]; then
+        min="$i"
+    fi
+done
+
+echo "Max is: $max"
+echo "Min is: $min"
